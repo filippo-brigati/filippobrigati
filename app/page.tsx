2dfa5eb497e4a91c3d519a15e16d6e984cdee965
Filippo Brigati <filippobrigati2@gmail.com>
2023-12-18 23:27:09 +0100

    refactor: blog using MD, updated packages version

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,23 @@
+import { allPosts } from "@/.contentlayer/generated";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <div className="w-full flex flex-col gap-y-5 leading-7 text-slate-800 dark:text-gray-200 my-9">
-        <p>
-          I’m a student and developer living in Reggio Emilia, currently
-          building software at GAB Tamagnini and other side projects. I’m
-          interested in a wide range of topics, including web development, open
-          source, hiking, climbing, cooking and ski.
-        </p>
-        <p>
-          Since high school, I spent many time coding, now I use Angular at
-          work. However, I am also familiar with React and Next. Currently, I am
-          pursuing a computer science course at University of Bologna to further
-          enhance my knowledge in this field.
-        </p>
-        <p>
-          You can know more about me via my writings and social media accounts.
-        </p>
+    <div className="mt-12">
+      <div className="flex flex-col gap-7 sm:gap-4">
+        {allPosts.map((post) => (
+          <Link
+            href={post.slug}
+            className="-mx-3 flex flex-col rounded-md px-3 no-underline hover:bg-[#F5F4F4] dark:hover:bg-zinc-800 sm:py-3"
+            key={post._id}
+          >
+            <span>{post.title}</span>
+            {post.description && (
+              <span className="text-gray-600 dark:text-gray-500">{post.description}</span>
+            )}
+          </Link>
+        ))}
       </div>
-      <hr className="my-12 h-0.5 border-t-0 bg-gray-200 dark:bg-gray-600 opacity-100 dark:opacity-50" />
-      <div className="w-full flex flex-row gap-x-4 leading-7 text-gray-200 my-9">
-        <a href="https://twitter.com/brigati_filippo" target="_blank" className=" no-underline text-slate-800 hover:text-slate-500 dark:text-white dark:hover:text-gray-500">Twitter</a>
-        <a href="https://github.com/filippo-brigati" target="_blank" className=" no-underline text-slate-800 hover:text-slate-500 dark:text-white dark:hover:text-gray-500">GitHub</a>
-        <a href="https://www.instagram.com/fillobrigati/" target="_blank" className=" no-underline text-slate-800 hover:text-slate-500 dark:text-white dark:hover:text-gray-500">Instagram</a>
-        <a href="https://www.linkedin.com/in/filippo-brigati-6569981b2/" target="_blank" className=" no-underline text-slate-800 hover:text-slate-500 dark:text-white dark:hover:text-gray-500">LinkedIn</a>
-      </div>
-    </>
+    </div>
   );
 }
